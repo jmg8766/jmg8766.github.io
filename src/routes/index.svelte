d6074955bb755646c0100aa2e67a6132a2dbc626
jmg8766 <justin.gottshall@gmail.com>
2022-06-17 18:20:24 -0400

    make icons bigger on mobile

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,29 +10,15 @@
 
 <div class="flex items-center justify-center h-screen">
 	<div class="inline-block tracking-wide text-center text-zinc-800">
-		<h1 class="mb-2 text-4xl font-semibold tracking-wide sm:text-7xl">Justin Gottshall</h1>
-		<div class="items-center justify-center h-8 sm:flex">
-			<p class="text-xl font-thin tracking-wide sm:inline-block sm:text-4xl">
-				Full-Stack Developer
-			</p>
-			<div class="inline-flex mt-4 sm:mt-0">
-				<a href="https://github.com/jmg8766" target="_blank" rel="noopener noreferrer">
-					<img
-						class="h-12 sm:ml-2 sm:h-8 hover:animate-spin"
-						width={32}
-						height={32}
-						alt="Github"
-						src={github}
-					/>
+		<h1 class="mb-2 text-5xl font-semibold tracking-wide sm:text-7xl">Justin Gottshall</h1>
+		<div class="items-center justify-center sm:h-8 sm:flex">
+			<p class="text-2xl font-thin tracking-wide sm:inline-block sm:text-4xl">Full-Stack Developer</p>
+			<div class="inline-flex mt-8 sm:mt-0">
+				<a class="mx-4 sm:mx-2" href="https://github.com/jmg8766" target="_blank" rel="noopener noreferrer">
+					<img class="w-12 sm:w-8 hover:animate-spin" width={32} height={32} alt="Github" src={github} />
 				</a>
-				<a href="mailto:justin.gottshall@gmail.com" target="_blank" rel="noopener noreferrer">
-					<img
-						class="h-12 ml-4 sm:ml-2 sm:h-8 hover:animate-spin"
-						width={32}
-						height={32}
-						alt="Email"
-						src={email}
-					/>
+				<a class="mx-4 sm:mx-2" href="mailto:justin.gottshall@gmail.com" target="_blank" rel="noopener noreferrer">
+					<img class="w-12 sm:w-8 hover:animate-spin" width={32} height={32} alt="Email" src={email} />
 				</a>
 			</div>
 		</div>
