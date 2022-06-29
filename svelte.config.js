4263bda0d4e1036f613f068b4e0ad7bbcae6b75b
jmg8766 <justin.gottshall@gmail.com>
2022-06-29 10:34:51 -0400

    disable js, inline styles

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,19 @@ import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess({ postcss: true }),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		// disable js, not needed at this time
+		...(process.env.NODE_ENV === 'production'
+			? {
+					browser: {
+						hydrate: false,
+						router: false
+					}
+			  }
+			: {}),
+		inlineStyleThreshold: Infinity
+	}
 };
 
 export default config;
