3f1227da0be5953ce7a2cf075c00b6b7fb1a39a4
jmg8766 <justin.gottshall@gmail.com>
2022-11-25 13:25:46 -0500

    path base

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess({ postcss: true }),
 	kit: {
 		adapter: adapter(),
-		inlineStyleThreshold: Infinity
+		inlineStyleThreshold: Infinity,
+		paths: { base: dev ? '' : '/personal-site' }
 	}
 };
 
