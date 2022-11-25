891ee35a00c2f87b35d0a30b005bcd17569a8686
jmg8766 <justin.gottshall@gmail.com>
2022-11-25 12:07:41 -0500

    not necessary

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const dev = process.argv.includes('dev');
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess({ postcss: true }),
 	kit: {
 		adapter: adapter(),
-		inlineStyleThreshold: Infinity,
-		paths: {
-			base: dev ? '' : '/personal-site'
-		}
+		inlineStyleThreshold: Infinity
 	}
 };
 
